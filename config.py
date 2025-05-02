2b1fdc40e0da67b1122a8dbfacbe39add43a498f
Your Name <you@example.com>
2025-05-01 21:22:55 -0300

    Refactor config.py to remove unused logging import and enhance S3 environment variable handling. Improved formatting in the validate_env_vars function for better readability.

--- config.py
+++ config.py
@@ -15,9 +15,8 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
-
 import os
-import logging
+# import logging # Commented out unused import
 
 # Retrieve the API key from environment variables
 API_KEY = os.environ.get('API_KEY')
@@ -31,15 +30,26 @@ LOCAL_STORAGE_PATH = os.environ.get('LOCAL_STORAGE_PATH', '/tmp')
 GCP_SA_CREDENTIALS = os.environ.get('GCP_SA_CREDENTIALS', '')
 GCP_BUCKET_NAME = os.environ.get('GCP_BUCKET_NAME', '')
 
-def validate_env_vars(provider):
+# S3 environment variables
+S3_ENDPOINT_URL = os.environ.get('S3_ENDPOINT_URL', '')
+S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY', '')
+S3_SECRET_KEY = os.environ.get('S3_SECRET_KEY', '')
+S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME', '')
+S3_REGION = os.environ.get('S3_REGION', 'us-east-1')
 
-    """ Validate the necessary environment variables for the selected storage provider """
+def validate_env_vars(provider):
+    """Validate the necessary environment variables for the selected storage provider"""
     required_vars = {
         'GCP': ['GCP_BUCKET_NAME', 'GCP_SA_CREDENTIALS'],
-        'S3': ['S3_ENDPOINT_URL', 'S3_ACCESS_KEY', 'S3_SECRET_KEY', 'S3_BUCKET_NAME', 'S3_REGION'],
+        'S3': ['S3_ENDPOINT_URL', 'S3_ACCESS_KEY', 'S3_SECRET_KEY',
+               'S3_BUCKET_NAME', 'S3_REGION'],
         'S3_DO': ['S3_ENDPOINT_URL', 'S3_ACCESS_KEY', 'S3_SECRET_KEY']
     }
-    
-    missing_vars = [var for var in required_vars[provider] if not os.getenv(var)]
+
+    missing_vars = [var for var in required_vars[provider]
+                    if not os.getenv(var)]
     if missing_vars:
-        raise ValueError(f"Missing environment variables for {provider} storage: {', '.join(missing_vars)}")
+        raise ValueError(
+            f"Missing environment variables for {provider} storage: "
+            f"{', '.join(missing_vars)}"
+        )
